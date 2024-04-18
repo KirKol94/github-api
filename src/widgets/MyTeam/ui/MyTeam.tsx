7c223ed6718361cae2864abc1103af48ea4daee6
kirkol94 <kirkol94@ya.ru>
2024-04-18 17:52:25 +0500

    MyTeam
    - Сортировка

--- src/widgets/MyTeam/ui/MyTeam.tsx
+++ src/widgets/MyTeam/ui/MyTeam.tsx
@@ -1,16 +1,41 @@
 import { myTeamStore } from '@/features/MyTeam'
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove'
+import SortByAlphaIcon from '@mui/icons-material/SortByAlpha'
 import { Avatar, Box, IconButton, Link, List, ListItem, Tooltip, Typography } from '@mui/material'
 import { observer } from 'mobx-react-lite'
-import PersonRemoveIcon from '@mui/icons-material/PersonRemove'
+import { useState } from 'react'
 
 export const MyTeam = observer(() => {
   const { myTeammates, removeTeammate } = myTeamStore
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc') // Состояние для хранения порядка сортировки
+
+  const handleSortClick = () => {
+    setSortOrder(prev => (prev === 'asc' ? 'desc' : 'asc'))
+  }
+
+  const sortedUsers = myTeammates ? [...myTeammates] : [] // Копируем массив пользователей
+
+  sortedUsers.sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return a.login.localeCompare(b.login)
+    } else {
+      return b.login.localeCompare(a.login)
+    }
+  })
 
   return (
     <Box>
-      <Typography variant="h2">My Team</Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center', borderBottom: '2px solid black' }}>
+        <Typography variant="h2">My Team</Typography>
+        <Tooltip title="Change sort order">
+          <IconButton onClick={handleSortClick}>
+            <SortByAlphaIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+
       <List>
-        {myTeammates.map(teammate => (
+        {sortedUsers.map(teammate => (
           <ListItem key={teammate.id} sx={{ pl: 0, display: 'flex', justifyContent: 'space-between' }}>
             <Link href={teammate.html_url} sx={{ color: 'inherit', display: 'flex', alignItems: 'center' }}>
               <Avatar alt={teammate.login} src={teammate.avatar_url} sx={{ width: 50, height: 50 }} />
--- src/widgets/UserList/ui/UserList.tsx
+++ src/widgets/UserList/ui/UserList.tsx
@@ -1,5 +1,4 @@
 import { Loader } from '@/shared/ui/Loader'
-import SortByAlphaIcon from '@mui/icons-material/SortByAlpha'
 import { Avatar, Box, IconButton, Link, List, ListItem, TextField, Tooltip, Typography } from '@mui/material'
 import { observer } from 'mobx-react-lite'
 import { useEffect, useState } from 'react'
@@ -11,9 +10,8 @@ export const UserList = observer(() => {
   const { fetchUsers, users, error, isLoading } = userListDataStore
   const { addTeammate, myTeammates } = myTeamStore
   const [searchValue, setSearchValue] = useState('')
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc') // Состояние для хранения порядка сортировки
 
-  const filterSortedList = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const filterList = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
     setSearchValue(value)
   }
@@ -21,22 +19,6 @@ export const UserList = observer(() => {
   useEffect(() => {
     fetchUsers('https://api.github.com/users?since=50000000')
   }, [fetchUsers])
-
-  const handleSortClick = () => {
-    // Изменяем состояние порядка сортировки при клике на кнопку
-    setSortOrder(prevOrder => (prevOrder === 'asc' ? 'desc' : 'asc'))
-  }
-
-  const sortedUsers = users ? [...users] : [] // Копируем массив пользователей
-
-  // Сортировка пользователей по алфавиту в соответствии с текущим порядком сортировки
-  sortedUsers.sort((a, b) => {
-    if (sortOrder === 'asc') {
-      return a.login.localeCompare(b.login)
-    } else {
-      return b.login.localeCompare(a.login)
-    }
-  })
 
   if (isLoading) return <Loader />
   if (error) return <Typography variant="h1">{error}</Typography>
@@ -60,18 +42,13 @@ export const UserList = observer(() => {
           }}
         >
           <Typography variant="h2">Users</Typography>
-          <Tooltip title="Change sort order">
-            <IconButton onClick={handleSortClick}>
-              <SortByAlphaIcon />
-            </IconButton>
-          </Tooltip>
         </Box>
 
-        <TextField label="Search" variant="outlined" type="search" onChange={filterSortedList} value={searchValue} />
+        <TextField label="Search" variant="outlined" type="search" onChange={filterList} value={searchValue} />
       </Box>
 
       <List>
-        {sortedUsers
+        {users
           .filter(user => user.login.toLowerCase().includes(searchValue.toLowerCase()))
           .map(user => (
             <ListItem key={user.id} sx={{ px: 0, display: 'flex', justifyContent: 'space-between' }}>
