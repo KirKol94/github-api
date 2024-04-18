39b3ba76388c585eb608f606384965896e15f83e
kirkol94 <kirkol94@ya.ru>
2024-04-18 08:45:48 +0500

    header
    - подсветка активной ссылки

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,14 +1,16 @@
 import { Box, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { headerLinks } from '../model/data/headerLinks'
 
 export const Header = () => {
+  const { pathname } = useLocation()
+
   return (
     <Box component="header" sx={{ borderBottom: '2px solid black', bgcolor: 'black' }}>
       <Box component="div" sx={{ display: 'flex' }}>
         {headerLinks.map(link => (
           <Link key={link.path} to={link.path}>
-            <Typography variant="body1" sx={{ py: 3, px: 2, color: 'white' }}>
+            <Typography variant="body1" sx={{ py: 3, px: 2, color: pathname === link.path ? 'primary' : 'white' }}>
               {link.name}
             </Typography>
           </Link>
