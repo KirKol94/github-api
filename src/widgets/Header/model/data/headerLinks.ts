4715be3121b1db0e441a8bf216003135aabc0fe5
kirkol94 <kirkol94@ya.ru>
2024-04-18 10:41:59 +0500

    header
    - иконки для ссылок

--- src/widgets/Header/model/data/headerLinks.ts
+++ src/widgets/Header/model/data/headerLinks.ts
@@ -1,16 +1,22 @@
+import GroupIcon from '@mui/icons-material/Group'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+
 type HeaderLink = {
   name: string
   path: string
+  Icon?: typeof GroupIcon
 }
 
 export const headerLinks: HeaderLink[] = [
   {
     name: 'Home',
     path: '/',
+    Icon: AccountCircleIcon,
   },
   {
     name: 'Team',
     path: '/team',
+    Icon: GroupIcon,
   },
   {
     path: '/notFoundPage',
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -10,9 +10,14 @@ export const Header = () => {
       <Box component="div" sx={{ display: 'flex' }}>
         {headerLinks.map(link => (
           <Link key={link.path} to={link.path}>
-            <Typography variant="body1" sx={{ py: 3, px: 2, color: pathname === link.path ? 'primary' : 'white' }}>
-              {link.name}
-            </Typography>
+            <Box
+              sx={{ px: 2, display: 'flex', alignItems: 'center', color: pathname === link.path ? 'primary' : 'white' }}
+            >
+              {link.Icon && <link.Icon />}
+              <Typography variant="body1" sx={{ py: 3, pl: 1 }}>
+                {link.name}
+              </Typography>
+            </Box>
           </Link>
         ))}
       </Box>
