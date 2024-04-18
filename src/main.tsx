16df6af20e53c192ac453b8bbcd1dbc2e0b5f8da
kirkol94 <kirkol94@ya.ru>
2024-04-18 17:09:15 +0500

    обновил package.json
    - при билде добавляется файл для корректной маршрутизации на netlify

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './styles/reset.css'
+import { App } from './app/App'
+import './app/styles/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
