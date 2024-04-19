9dc7b8d4f7d08dfec9c7bd911fac01d0df251735
kirkol94 <kirkol94@ya.ru>
2024-04-19 20:37:05 +0500

    Стили для системной темы
    - цвет фона
    - цвет текст

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import ReactDOM from 'react-dom/client'
 import { App } from './app/App'
-import './app/styles/reset.css'
+import './app/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
